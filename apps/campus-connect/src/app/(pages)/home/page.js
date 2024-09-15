05073b24d5f5c51500a79c525388aa8d05725cc3
kartik7454 <kb848067@gmail.com>
2024-09-15 23:29:00 +0530

    image2

--- apps/campus-connect/src/app/(pages)/home/page.js
+++ apps/campus-connect/src/app/(pages)/home/page.js
@@ -11,16 +11,7 @@ export default function Home() {
     
     return (
       <div>
-       <img
-          className='h-80 w-48 object-cover rounded-lg '
-        src={"/images/"+"download.jpeg"}
-    
        
-        alt={"img"}
-        
-        
-        
-        />
       <div><h1 className="text-9xl absolute top-44 z-50 text-white font-bold text-center font-sans">Connect. Engage. Discover</h1>
         <h1 className="text-2xl absolute top-96 mt-28 z-50 text-white font-bold text-center font-mono px-44">Campus Connect is the ultimate platform for students to discover and participate in exciting campus events.</h1></div>
         
